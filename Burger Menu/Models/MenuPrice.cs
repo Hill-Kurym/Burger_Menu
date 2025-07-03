5d096a7fb88b6331a44e927d357982345be44fae
dasha kraus <goshap34@gmail.com>
2025-07-03 08:49:15 +0500

    Добавил логику итогового ценника

---
+++ Burger Menu/Models/MenuPrice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burger_Menu.Models
+{
+    public class MenuPrice
+    {
+        /// <summary>
+        /// Создадим словарь для хранения названия продуктов и их стоимости.
+        /// Приватный, чтобы было видно только изнутри класса. Другим классам нет нужды к нему обращаться
+        /// Readonly, чтобы зафиксировать элементы словаря, без возможности изменить
+        /// Ключ (string) - название продукта. Значение (double) - цена продукта
+        /// Модификатор OrdinalIgnoreCase позволит обращаться к словарю без учета регистра
+        /// </summary>
+        private readonly Dictionary<string, double> _menu = new Dictionary<string, double>(StringComparer.OrdinalIgnoreCase)
+        {
+            // Сразу добавим элементы в наше меню. Является плохим практисом, но допустимо в маленьких проектах 
+            ["Burger"] = 200,
+            ["Drink"] = 100,
+
+            // Добавь сюда элементы, если хочешь расширить меню
+        };
+
+        /// <summary>
+        /// Возвращает значение словаря по найденному ключу
+        /// </summary>
+        /// <param name="name">Ключ словаря</param>
+        /// <returns></returns>
+        public double? GetPrice(string name)
+        {
+            if (_menu.TryGetValue(name, out var price)) // Важно использовать Try ( TryGetValue, TryAdd, TryDequeue ), дабы не выносить exeption за пределы вызова
+                return price;
+            return null;
+        }
+    }
+}
--- Burger Menu/ViewModels/MainWindowViewModel.cs
+++ Burger Menu/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,64 @@
-﻿namespace Burger_Menu.ViewModels
+﻿using Burger_Menu.Models;
+using ReactiveUI;
+using System;
+using System.Reactive;
+
+namespace Burger_Menu.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting { get; } = "Welcome to Avalonia!";
+
+
+
+        /// <summary>
+        ///  Инициализация команды. 
+        ///  Изменил тип передаваемого значения с общего на строковый 
+        ///  Теперь при использовании command можно добавить параметр ( строку )
+        /// </summary>
+        public ReactiveCommand<string, Unit> ButtonClick { get; }
+
+
+
+        /// <summary>
+        /// Определяем переменную для вывода итогового ценника
+        /// Обязательно используем паттерн MVVM для обеспечения доступа к локальной переменной
+        /// Не забывает пользоваться функциями ReactiveUI для обновления интерфейса. Метод RaiseAndSetIfChanged - 
+        ///    - вызывает Notice и уведомляет интерфейс о необходимости перерисоваться
+        /// </summary>
+        private string _price = "0"; // Переменная
+        public string Price    // Поле
+        {
+            get => _price;
+            set => this.RaiseAndSetIfChanged(ref _price, value); // Уведомляем интерфейс, при изменении поля
+        }
+
+
+
+        /// <summary>
+        /// В конструторе класса мы всегда инициализируем комманды ( крайне редко нет )
+        /// Тк он имеет высокое место в иерархии вызовов и вызывается до отрисовки UI. Так же можно использовать конструктор cs файла страницы
+        /// </summary>
+        public MainWindowViewModel()
+        {
+            /// Логика должна быть вынесена на model
+            /// ButtonClick = ReactiveCommand.Create<string>(метод);
+            /// Но если работа слишком маленькая ( изменение локальной переменной ), то можно и выполнить ее во ViewModel
+            ButtonClick = ReactiveCommand.Create<string>(Calc);
+
+        }
+
+
+        
+        /// <summary>
+        /// Вот уже наше имя продукта попадает в метод логики. Я разместил его во ViewModel, тк он слишком маленький.
+        /// </summary>
+        /// <param name="name"></param>
+        private void Calc(string name)
+        {
+            MenuPrice menu = new MenuPrice();
+
+            Price = (Convert.ToDouble(Price) + menu.GetPrice(name)).ToString();
+        }
     }
 }
